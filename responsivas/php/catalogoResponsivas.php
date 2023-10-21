3f4526df85e07642d10b91067421cd692c949ce8
JOSShelby <jo0zue99@hotmail.com>
2023-10-20 20:20:38 -0600

    catalogo responsivas

--- responsivas/php/catalogoResponsivas.php
+++ responsivas/php/catalogoResponsivas.php
@@ -1,4 +1,9 @@
 <?php
+    // TRAE LOS PRODUCTOS
+    $conObtenerResponsivas = new conexion;
+    $queryObtenerResponsivas = "SELECT r.idresponsiva, u.nombreusuario, r.fechacreacion, r.banderafirmado, r.estadoid
+    FROM responsivas r, usuarios u WHERE u.idusuario = r.usuarioid";
+    $resultados = $conObtenerResponsivas->conn->query($queryObtenerResponsivas);
 ?>
 
 <div class="row" id="catalogoResponsivas" style="display: none;">
@@ -11,22 +16,36 @@
     <div class="col-1"></div>
 
     <div class="col-10">
-        <!-- TABLA DONDE APARECEN LOS PRODUCTOS -->
+        <!-- TABLA DONDE APARECEN LAS RESPONSIVAS -->
         <div class="table-responsive">
             <table id="catalogoProductos" class="table table-hover">
                 <!-- TITULOS DE LAS COLUMNAS -->
                 <thead>
                     <tr class="sticky-top">
-                        <th class="text-center" scope="col">Numero de Parte</th>
-                        <th class="text-center" scope="col">Existentes</th>
-                        <th class="text-center" scope="col">Precio Por Unidad</th>
-                        <th class="text-center" scope="col">Foto</th>
-                        <th class="text-center" colspan="2" scope="col">Acción</th>
+                        <th class="text-center" scope="col">Usuario creador</th>
+                        <th class="text-center" scope="col">Fecha de creacion</th>
+                        <th class="text-center" scope="col">Firmado</th>
                     </tr>
                 </thead>
                 <!-- CONTENIDO DE LA TABLA -->
                 <tbody>
                     <img class="marcaAguaTabla" src="../../src/imagenes/logo.png">
+                    <?php
+                        foreach ($resultados->fetch_all() as $responsiva) {
+                            echo "  <tr>
+                                        <td class=\"text-center\">" . $responsiva[1] . "</td>
+                                        <td class=\"text-center\">" . $responsiva[2] . "</td>";
+                                        
+                                        if($responsiva[3] == 0){
+                                            $firmado = 'No';
+                                        } else {
+                                            $firmado = 'Si';
+                                        }
+
+                            echo "      <td class=\"text-center\">" . $firmado . "</td>";
+                            echo "  </tr>";
+                        }
+                    ?>
                 </tbody>
             </table>
         </div>
