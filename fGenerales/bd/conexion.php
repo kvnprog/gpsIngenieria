b23f5694ea95fb2f42f220795311d4dbe2883d9d
kevin.alvarez <kevin.alvarez@agrizar.com>
2023-12-27 12:56:55 -0600

    Conexion bd hostinger

--- fGenerales/bd/conexion.php
+++ fGenerales/bd/conexion.php
@@ -9,7 +9,7 @@ class conexion{
 
     function __construct(){
         //local
-        $this->conn = new mysqli("localhost","root","","gpsingenieria",3306);
+        $this->conn = new mysqli("localhost","u798288314_admin","Contrasen@2023","u798288314_gpsingenieria",3306);
         //pruebas
         //$this->conn = new mysqli("185.27.134.10","if0_34641860","","if0_34641860_gpsingenieria",3306);
     }
